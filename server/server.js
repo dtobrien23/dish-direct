98912e7ba4834bec386c06fe3c31a3c98737aeed
Dean <dtobrien23@gmail.com>
2024-04-04 15:55:42 +0100

    Init commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+require("dotenv").config({ path: "../.env" });
+
+const spoonURL = "https://api.spoonacular.com/recipes/complexSearch";
+const spoonApiKey = process.env.SPOON_API_KEY;
+
+app.get("/get-recipe", async (req, res) => {
+  try {
+    // Make a request to the Spoonacular API using Axios
+    const response = await axios.get(spoonURL, {
+      params: {
+        apiKey: spoonApiKey,
+        query: "pasta",
+        maxFat: 25,
+        number: 100,
+      },
+    });
+    const data = response.data;
+
+    // Send the response back to the client
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    res.status(500).json({ error: "An error occurred while fetching recipes" });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
