9e45efbf99830eeadcc0531c406c43fb6e010a97
Dean <dtobrien23@gmail.com>
2024-04-10 21:11:32 +0100

    Add search bar for client to specify cuisine

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [recipeData, setRecipeData] = useState([{}]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [randomIndex, setRandomIndex] = useState();
 
-  const randomIndex = Math.floor(Math.random() * 100);
-
-  function handleClick() {
-    fetch("/get-recipe")
+  function handleSearch() {
+    // prevents default form submission behaviour
+    console.log(searchQuery);
+    fetch("/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: searchQuery }),
+    })
       .then((response) => response.json())
       .then((data) => {
         setRecipeData(data);
         console.log(data);
+        {
+          data.number > data.totalResults
+            ? setRandomIndex(Math.floor(Math.random() * data.totalResults))
+            : setRandomIndex(Math.floor(Math.random() * data.number));
+        }
       });
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Click me for a pasta recipe!</button>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSearch();
+        }}
+      >
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(event) => {
+            event.preventDefault();
+            setSearchQuery(event.target.value);
+          }}
+          placeholder="Enter the type of food you want a recipe for..."
+        />
+        <button type="submit">Search</button>
+      </form>
       {typeof recipeData.number === "undefined" ? (
         <p>No recipe yet...</p>
       ) : (
         <div>
           <p>{recipeData.results[randomIndex].title}</p>
-          <img
-            src={recipeData.results[randomIndex].image}
-            alt="Lovely fooooood"
-          />
+          <img src={recipeData.results[randomIndex].image} alt="Lovely food" />
         </div>
       )}
     </div>
--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser"); // needed to parse JSON data
 const app = express();
 
 require("dotenv").config({ path: "../.env" });
 
 const spoonURL = "https://api.spoonacular.com/recipes/complexSearch";
 const spoonApiKey = process.env.SPOON_API_KEY;
+const jsonParser = bodyParser.json();
 
-app.get("/get-recipe", async (req, res) => {
+app.post("/search", jsonParser, async (req, res) => {
   try {
+    const searchQuery = req.body.query;
+    console.log("hellooooo", searchQuery);
     // Make a request to the Spoonacular API using Axios
     const response = await axios.get(spoonURL, {
       params: {
         apiKey: spoonApiKey,
-        query: "pasta",
+        query: searchQuery,
         maxFat: 25,
         number: 100,
       },
@@ -21,6 +25,7 @@ app.get("/get-recipe", async (req, res) => {
     const data = response.data;
 
     // Send the response back to the client
+    console.log(data);
     res.json(data);
   } catch (error) {
     console.error("Error fetching recipes:", error);
